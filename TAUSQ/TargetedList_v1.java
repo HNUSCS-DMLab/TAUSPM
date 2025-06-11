9fa6d8980c53fec9e78ed1e76aa865f65c22cdcc
HNUKC <127593272+HNUSCS-DMLab@users.noreply.github.com>
2025-06-11 09:11:41 +0800

    Add files via upload

---
+++ TAUSQ/TargetedList_v1.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C), 2015-2025, HNU
+ * FileName: UtilityList_New
+ * Author:   K. Cao
+ * Date:     2025/6/1 08:00
+ * Description: Add the new domains "IMatch", "IIMatch", "rru", "rrl" into UtilityList.
+ */
+package TAUSQ;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TargetedList {
+    public class UtilityElement {
+
+        /** itemset  **/
+        public int tid;
+
+        /** prefix utility **/
+        public int acu;
+
+        /** rrs utility **/
+        public int rru;
+        
+        /** rrs length **/
+        public int rrl;
+        
+        public UtilityElement(int tid, int acu, int rru, int rrl) {
+            this.tid=tid;
+            this.acu=acu;
+            this.rru=rru;
+            this.rrl=rrl;
+        }
+    }
+
+    //sid
+    public int sid;
+
+    //IIMatch：Item flag for current match
+    public int IIMatch;
+    //IMatch：Itemset flag for current match
+    public int IMatch;
+
+    //UtilityList: for per instance
+    List<UtilityElement> List = new ArrayList<UtilityElement>();
+
+    //length: number of instances
+    public int LengthOfUtilityList;
+
+    //SRAU：Suffix Remain Average Utility
+    public int SRAU;
+
+    public TargetedList() {
+        this.LengthOfUtilityList=0;
+        this.SRAU = 0;
+    }
+
+    public void add(int tid, int acu, int rru, int rrl) {
+        this.List.add(new UtilityElement(tid, acu, rru, rrl));
+        this.LengthOfUtilityList++;
+    }
+
+    public void set_SRAU(int srau) {
+      this.SRAU = srau;
+    }
+
+    public void set_sid(int sid) {
+        this.sid = sid;
+    }
+
+    public int get_sid() {
+        return this.sid;
+    }
+
+    public void set_IIMatch(int IIMatch) {
+        this.IIMatch = IIMatch;
+    }
+    public void set_IMatch(int IMatch) {
+        this.IMatch = IMatch;
+    }
+    public int get_IIMatch() {
+        return this.IIMatch;
+    }
+    public int get_IMatch() {
+        return this.IMatch;
+    }
+}
